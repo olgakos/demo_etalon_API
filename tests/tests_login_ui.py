3112b3037883cacc54ea30c083e39c9ca563140c
olgakos <ok2304@yandex.ru>
2023-09-03 19:22:31 +0300

    api + ui

--- tests/tests_login_ui.py
+++ tests/tests_login_ui.py
@@ -2,6 +2,7 @@ from selene import browser, have, be
 
 login = 'arven_test@etalongroup.com'
 password = '8Y*m8DthaeSn'
+password_bad = '123456'
 
 def test_login_ui():
     browser.open('/')
@@ -11,6 +12,21 @@ def test_login_ui():
     browser.element('.btn-primary').click()
     browser.element('#sidebar').should(have.text('Ундомиэль Арвен null'))
 
+def test_login_ui_bad():
+    browser.open('/')
+    browser.element('.form-login').should(have.text('Авторизация'))
+    browser.element('[name=email]').type(login)
+    browser.element('#passInput').type(password_bad)
+    browser.element('.btn-primary').click()
+    browser.element('.toast').should(have.text('проверьте правильность логина и пароля'))
+
+def test_login_ui_bad_username():
+    browser.open('/')
+    browser.element('.form-login').should(have.text('Авторизация'))
+    browser.element('[name=email]').type(login)
+    browser.element('#passInput').type(password)
+    browser.element('.btn-primary').click()
+    browser.element('#sidebar').should(have.no.text('Фродо Бэггинс'))
 
 def test_projects_page_available():
     browser.open('/')
@@ -22,6 +38,3 @@ def test_projects_page_available():
     browser.open('#!/app/core/projects///')
     browser.element('.skks-SPB').should(be.visible)
     browser.element('.skks-Moscow').should(be.visible)
-
-
-
